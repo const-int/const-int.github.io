3b361a3a74621bf3a2cbcd9890f00bbc6fddac8b
Konstantin Naumov <const@MacMachine.local>
2015-01-25 19:07:58 +0200

    123

--- _site/assets/js/navigation.js
+++ _site/assets/js/navigation.js
@@ -21,7 +21,8 @@ $(document).ready(function() {
 
     if (!active_link.hasClass('active')) {
 
-      history.pushState(null, null, $(active_link).attr('href'));
+      // history.pushState(null, null, $(active_link).attr('href'));
+      History.pushState(null, null, $(active_link).attr('href'));
 
       // Get page #
       nav_index = active_link.index();
@@ -33,19 +34,35 @@ $(document).ready(function() {
   });
 
 
-  $(window).on("popstate load", function() {
+  // Bind to StateChange Event
+    History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+        var State = History.getState(); // Note: We are using History.getState() instead of event.state
+        var url_array = document.URL.split('/'),
+        hash = url_array[url_array.length - 1],
+        nav_index = ['#about', '#portfolio', '#experience', '#contact'].indexOf(hash),
+        block = $(".full-screen-block").eq(nav_index);
 
-      var url_array = document.URL.split('/'),
-      hash = url_array[url_array.length - 1],
-      nav_index = ['#about', '#portfolio', '#experience', '#contact'].indexOf(hash),
-      block = $(".full-screen-block").eq(nav_index);
+        console.log(nav_index);
 
-      console.log(nav_index);
+        if ( (hash == '#about' || hash == '') && times_moved == 0) { return }
 
-      if ( (hash == '#about' || hash == '') && times_moved == 0) { return }
+        move_pages(block, nav_index);
+    });
 
-      move_pages(block, nav_index);
-  });
+
+  // $(window).on("popstate load", function() {
+
+  //     var url_array = document.URL.split('/'),
+  //     hash = url_array[url_array.length - 1],
+  //     nav_index = ['#about', '#portfolio', '#experience', '#contact'].indexOf(hash),
+  //     block = $(".full-screen-block").eq(nav_index);
+
+  //     console.log(nav_index);
+
+  //     if ( (hash == '#about' || hash == '') && times_moved == 0) { return }
+
+  //     move_pages(block, nav_index);
+  // });
 
 
 
